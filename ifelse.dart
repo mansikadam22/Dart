6d7f52c619d0b0cf3c3f5fca3dab9e5e71a5e198
mansi kadam <kadammansi455@gmail.com>
2025-06-10 00:06:49 -0700

    commit

---
+++ ifelse.dart
@@ -0,0 +1,23 @@
+void main()
+{
+  int day = 2;
+  if(day<=0)
+   {
+    print("no fine");
+   }
+    else if(day>=1 && day<=5)
+     {
+      print("fine amount is 2 rupees");
+     }else if(day>=6 && day<=10)
+     {
+      print("fine amount is 4 rupees");
+     }else if (day>=11 && day<=31)
+     {
+      print("fine amount is 6 rupees");
+     }
+   else
+    {
+      print("invalid day");
+    }
+
+}
